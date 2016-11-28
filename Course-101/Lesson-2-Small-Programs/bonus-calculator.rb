6781d1555202d64bcd509856b0fe7dae9b4add01
Colton Ehrman <cjames615@hotmail.com>
2016-11-28 00:27:17 -0600

    Can exit calculator using 'Q/q' or 'X/x'; Got rid of all comments in code and turned isolate_characters into a function; Refactored op to be a bit more easier to understand what is going on; Refactored op indeces

--- Course-101/Lesson-2-Small-Programs/bonus-calculator.rb
+++ Course-101/Lesson-2-Small-Programs/bonus-calculator.rb
@@ -4,41 +4,41 @@ def calc(num1, op, num2)
   num1 = num1.to_f
   num2 = num2.to_f
   case op
-  when '+' then num1 + num2
-  when '-' then num1 - num2
-  when '*', 'x' then num1 * num2
-  when '**' then num1**num2
-  when '/' then num1 / num2.to_f
+    when '+' then num1 + num2
+    when '-' then num1 - num2
+    when '*', 'x' then num1 * num2
+    when '**' then num1**num2
+    when '/' then num1 / num2.to_f
   end
 end
 
+def isolate_characters(string)
+  string.split('').reject { |char| char == ' ' }
+end
+
 loop do
-  Kernel.print 'Please enter an equation to calulate ("q" to quit): '
+  Kernel.print 'Please enter an equation to calulate ("q" or "x" to quit): '
 
-  # Break loop if "q" is entered
-  equation = Kernel.gets.chomp.downcase
-  break if equation == 'q'
+  equation = Kernel.gets.chomp
+  break if 'qx'.include? equation.downcase
 
-  # Isolate equation characters
-  equation = equation.split('').keep_if { |elem| elem != ' ' }
+  equation_chars = isolate_characters equation
+  equation = equation_chars.join('')
 
-  # Restart loop if an OPERATION is not present in equation
-  match = EQUATION_REGEX.match(equation.join(''))
+  match = EQUATION_REGEX.match(equation)
   if match.nil?
     Kernel.puts "\n\t**INVALID EQUATION WAS ENTERED!**\n\n"
     next
   end
 
-  # Get operation from regex match
-  op = match[1]
+  op = match.captures.first
 
-  # Get the index of the operation in the equation
-  op_start_index = match.begin(1)
-  op_end_index = op_start_index + op.size
+  op_start_index = match.offset(1)[0]
+  op_end_index = match.offset(1)[1]
 
-  first_operator = equation.slice(0, op_start_index).join('')
-  second_operator = equation.slice(op_end_index, equation.size - 1).join('')
+  first_operator = equation_chars.slice(0, op_start_index).join('')
+  second_operator = equation_chars.slice(op_end_index, equation.size - 1).join('')
 
-  Kernel.puts "#{first_operator} #{op} #{second_operator} =\
- #{calc(first_operator, op, second_operator)}"
+  Kernel.puts "=> #{first_operator} #{op} #{second_operator} =\
+ #{calc(first_operator, op, second_operator)}\n\n"
 end
