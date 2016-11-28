4f9a6b1cc339324507b5dfc59f76bb25d57e1ebf
Colton Ehrman <cjames615@hotmail.com>
2016-11-27 21:11:50 -0600

    Refactor after running code through rubocop

--- Course-101/Lesson-2-Small-Programs/calculator.rb
+++ Course-101/Lesson-2-Small-Programs/calculator.rb
@@ -1,42 +1,42 @@
-OPERATIONS_REGEX = /-?\d+(\+|-|x|\*|\/)-?\d+/
+EQUATION_REGEX = %r{-?\d+(\+|-|x|\*|\/)-?\d+}
 
-def calculate(num1, operation, num2)
-  num1 = num1.to_f
-  num2 = num2.to_f
-
-  case operation
-  when "+"
-    num1 + num2
-  when "-"
-    num1 - num2
-  when "*", "x"
-    num1 * num2.round(2)
-  when "/"
-    num1 / num2.round(2)
-  else
-    "Invalid operation"
+def calc(num1, op, num2)
+  num1 = num1.to_i
+  num2 = num2.to_i
+  case op
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  when '*', 'x' then num1 * num2
+  when '/' then num1 / num2.to_f
   end
 end
 
 loop do
-  print "Please enter an equation to calulate: "
+  Kernel.print 'Please enter an equation to calulate ("q" to quit): '
 
   # Break loop if "q" is entered
-  break if "q" == equation = Kernel.gets.chomp
+  equation = Kernel.gets.chomp
+  break if equation == 'q'
 
   # Isolate equation characters
-  equation = equation.split("").keep_if { |elem| elem != " " }
+  equation = equation.split('').keep_if { |elem| elem != ' ' }
 
   # Restart loop if an OPERATION is not present in equation
-  next if nil == match = OPERATIONS_REGEX.match(equation.join(""))
+  match = EQUATION_REGEX.match(equation.join(''))
+  if match.nil?
+    Kernel.puts "\n\t**INVALID EQUATION WAS ENTERED!**\n\n"
+    next
+  end
+
+  # Get operation from regex match
   op = match[1]
 
-  # Get the index of the OPERATION in the equation
+  # Get the index of the operation in the equation
   op_index = match.begin(1)
 
-  first_operator = equation.slice(0, op_index).join("")
-  second_operator = equation.slice(op_index + 1, equation.size - 1).join("")
+  first_operator = equation.slice(0, op_index).join('')
+  second_operator = equation.slice(op_index + 1, equation.size - 1).join('')
 
-  Kernel.puts "#{first_operator} #{op} #{second_operator} = #{calculate(first_operator, op, second_operator)}"
-
+  Kernel.puts "#{first_operator} #{op} #{second_operator} =\
+ #{calc(first_operator, op, second_operator)}"
 end
