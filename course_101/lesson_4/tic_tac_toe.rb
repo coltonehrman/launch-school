7fce9d61d0d6b40a7bcb9072555465120230201a
Colton Ehrman <cjames615@hotmail.com>
2016-12-03 23:07:53 -0600

    Tic tac toe refactor

--- course_101/lesson_4/tic_tac_toe.rb
+++ course_101/lesson_4/tic_tac_toe.rb
@@ -1,14 +1,27 @@
 require 'pry'
 
-BOARD_MAP = [%w(1 2 3), %w(4 5 6), %w(7 8 9)].freeze
-
 VERTICAL_CHAR   = '|'.freeze
 HORIZONTAL_CHAR = '-'.freeze
 INTERSECT_CHAR  = '#'.freeze
 CELL_PADDING    = 7 # Must be odd
 
-PLAYER_MARKER   = 'X'.freeze
-COMPUTER_MARKER = 'O'.freeze
+$player_marker   = nil
+$computer_marker = nil
+
+CORNERS = [1, 3, 7, 9].freeze
+OPPOSITE_CORNER = { 1 => 9, 3 => 7, 7 => 3, 9 => 1 }.freeze
+
+WINNING_GROUPS = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9],
+  [1, 4, 7], [2, 5, 8], [3, 6, 9],
+  [1, 5, 9], [3, 5, 7]
+].freeze
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = ' ' }
+  new_board
+end
 
 def print_empty_row
   print "#{' ' * CELL_PADDING}#{VERTICAL_CHAR}\
@@ -33,216 +46,179 @@ def print_border_rows(num)
   print "\n"
 end
 
-def print_spot(cell)
+def print_square(square)
   print "#{' ' * (CELL_PADDING / 2)}\
-#{cell.index(/[X|O]/) ? ' ' : cell}\
-#{' ' * (CELL_PADDING / 2)}"
-end
-
-def print_cell(cell)
-  print "#{' ' * (CELL_PADDING / 2)}\
-#{cell.index(/[X|O]/) ? cell : ' '}\
+#{square}\
 #{' ' * (CELL_PADDING / 2)}"
 end
 
 def print_board(row, type)
   print "\t"
-  row.each_with_index do |cell, cindex|
-    print_cell(cell) if type == :game
-    print_spot(cell) if type == :spots
-    print VERTICAL_CHAR unless cindex == 2
+  row.each_with_index do |col, index|
+    if type == :spots
+      col[1] == ' ' ? print_square(col[0]) : print_square(' ')
+    else
+      print_square(col[1])
+    end
+    print VERTICAL_CHAR unless index == 2
   end
   print "\t"
 end
 
-def print_board_titles(title1, title2)
+def print_board_titles(first, second)
   print "\n\t"
-  title1 = title1.center(CELL_PADDING * 3 + 2)
-  print "#{title1}\t\t"
-  title2 = title2.center(CELL_PADDING * 3 + 2)
-  print "#{title2}\n\n"
+  title = first.center(CELL_PADDING * 3 + 2)
+  print "#{title}\t\t"
+  title = second.center(CELL_PADDING * 3 + 2)
+  print "#{title}\n\n"
 end
 
-def display_boards(board)
-  board = board.each_slice(3).to_a
-
+def display_boards(brd)
   print_board_titles("GAME BOARD", "SPOTS LEFT")
-  board.each_with_index do |row, rindex|
+  rows_and_columns(brd).each_with_index do |row, index|
     print_empty_rows(2)
 
     print_board(row, :game)
     print_board(row, :spots)
-    print "\n"
+    puts "\n"
 
     print_empty_rows(2)
-    print_border_rows(2) unless rindex == 2
+    print_border_rows(2) unless index == 2
   end
   puts "\n"
 end
 
-def player_plays!(board)
+def rows_and_columns(brd)
+  brd.to_a.each_slice(3).to_a
+end
+
+def spots_left(brd)
+  brd.select { |_, v| v == ' ' }
+end
+
+def place_piece!(brd, player)
+  player_plays!(brd) if player == :player
+  computer_plays!(brd) if player == :computer
+end
+
+def player_plays!(brd)
   loop do
     print "Where would you like to play?\n\
 (choose a number from the SPOTS LEFT board on your right): "
-    answer = gets.chomp
+    answer = gets.chomp.to_i
 
-    unless board.include?(answer)
+    if brd[answer].nil?
       puts "** Invalid Choice **"
       puts "\n"
       next
     end
 
-    board[board.index(answer)] = PLAYER_MARKER
+    brd[answer] = $player_marker
     return
   end
 end
 
-def players_spots(board)
-  x_spots, o_spots = rows_and_columns(board)
-  PLAYER_MARKER == 'X' ? x_spots : o_spots
-end
+def spot_to_win(brd)
+  x_spot = nil
+  o_spot = nil
+  WINNING_GROUPS.each do |winning_squares|
+    values_at_squares = brd.values_at(*winning_squares)
+    squares_x = values_at_squares - ['X']
+    squares_o = values_at_squares - ['O']
 
-def computers_spots(board)
-  x_spots, o_spots = rows_and_columns(board)
-  COMPUTER_MARKER == 'X' ? x_spots : o_spots
-end
-
-def about_to_win?(board, spots)
-  rows = spots[:rows]
-  cols = spots[:columns]
-  return false if rows.empty? || cols.empty?
-  marker = board.each_slice(3).to_a[rows.first][cols.first]
-  other_marker = marker == 'X' ? 'O' : 'X'
-
-  closest_row = most_in_array(rows)
-  closest_col = most_in_array(cols)
-
-  ((get_row(board, closest_row) - [marker]).size == 1 && (!get_row(board, closest_row).include?(other_marker))) ||
-    ((get_column(board, closest_col) - [marker]).size == 1 && (!get_column(board, closest_col).include?(other_marker)))
-end
-
-def spot_to_win(board, spots)
-  rows = spots[:rows]
-  cols = spots[:columns]
-
-  marker = board.each_slice(3).to_a[rows.first][cols.first]
-  other_marker = marker == 'X' ? 'O' : 'X'
-
-  row_with_most = most_in_array(rows)
-  col_with_most = most_in_array(cols)
-
-  row_to_win = (get_row(board, row_with_most) - [marker])
-  col_to_win = (get_column(board, col_with_most) - [marker])
-
-  result = row_to_win.first if row_to_win.size == 1 && !row_to_win.include?(other_marker)
-  result = col_to_win.first if col_to_win.size == 1 && !col_to_win.include?(other_marker)
-  result
-end
-
-def get_row(board, row)
-  board.each_slice(3).to_a[row]
-end
-
-def row_is_full?(board, row)
-  row = get_row(board, row)
-  (row - [PLAYER_MARKER, COMPUTER_MARKER]).size.zero?
-end
-
-def get_column(board, column)
-  board.each_slice(3).to_a.map { |row| row[column] }
-end
-
-def col_is_full?(board, col)
-  col = get_column(board, col)
-  (col - [PLAYER_MARKER, COMPUTER_MARKER]).size.zero?
-end
-
-def computer_plays!(board)
-  spots_left = (board & BOARD_MAP.flatten)
-  player = players_spots(board)
-  computer = computers_spots(board)
-  spot = if about_to_win?(board, computer)
-           spot_to_win(board, computer)
-         elsif about_to_win?(board, player)
-           spot_to_win(board, player)
-         end
-  spot ||= spots_left.sample
-  board[board.index(spot)] = COMPUTER_MARKER
-end
-
-def board_full?(board)
-  (board & BOARD_MAP.flatten).empty?
-end
-
-def most_in_array(arr)
-  arr.max_by { |i| arr.count(i) }
-end
-
-def rows_and_columns(board)
-  x_spots = { rows: [], columns: [] }
-  o_spots = { rows: [], columns: [] }
-  board.each_slice(3).to_a.each_with_index do |row, rindex|
-    row.each_with_index do |column, cindex|
-      case column
-      when 'X'
-        x_spots[:rows] << rindex
-        x_spots[:columns] << cindex
-      when 'O'
-        o_spots[:rows] << rindex
-        o_spots[:columns] << cindex
-      end
+    if !squares_x.include?('O') && squares_x.size == 1
+      x_spot = winning_squares[values_at_squares.index(' ')]
+    end
+    if !squares_o.include?('X') && squares_o.size == 1
+      o_spot = winning_squares[values_at_squares.index(' ')]
     end
   end
-  [x_spots, o_spots]
+  { 'X' => x_spot, 'O' => o_spot } if x_spot || o_spot
 end
 
-def diagonal_win?(board)
-  diag_down = [0, 4, 8]
-  diag_up = [6, 4, 2]
-  (board.values_at(*diag_down).uniq.size == 1 ||
-    board.values_at(*diag_up).uniq.size == 1)
+def get_spot_from_value(brd, value)
+  brd.select { |_, v| v == value }.keys.last
 end
 
-def win?(spots)
-  rows = spots[:rows]
-  cols = spots[:columns]
-  (rows.count(most_in_array(rows)) >= 3 ||
-    cols.count(most_in_array(cols)) >= 3)
-end
-
-def someone_won?(board)
-  if board.count('X') >= 3 || board.count('O') >= 3
-    x_spots, o_spots = rows_and_columns(board)
-    win?(x_spots) || win?(o_spots) || diagonal_win?(board)
+def computer_plays!(brd)
+  spots = spot_to_win(brd)
+  spot = spots[$computer_marker] unless spots.nil?
+  spot ||= spots[$player_marker] unless spots.nil?
+  if spot.nil?
+    x_spot = get_spot_from_value(brd, $player_marker)
+    spot = if (brd.values - [$player_marker]).size == 8 && CORNERS.include?(x_spot)
+             5
+           elsif (brd.values - [$player_marker]).size == 7
+             (spots_left(brd).keys - CORNERS).sample
+           else
+             (CORNERS & spots_left(brd).keys).sample
+           end
   end
+  spot ||= spots_left(brd).keys.sample
+  brd[spot] = $computer_marker
 end
 
-def find_winner(board)
-  if someone_won?(board)
-    x_spots, o_spots = rows_and_columns(board)
-    if diagonal_win?(board) then return board[4]
-    elsif win?(x_spots) then return 'X'
-    elsif win?(o_spots) then return 'O'
-    end
+def find_winner(brd)
+  WINNING_GROUPS.each do |winning_squares|
+    values_at_squares = brd.values_at(*winning_squares)
+    return 'X' if values_at_squares == ['X', 'X', 'X']
+    return 'O' if values_at_squares == ['O', 'O', 'O']
   end
+  nil
 end
 
-def play_game
-  game_board = BOARD_MAP.flatten
+def board_full?(brd)
+  (brd.values - [$player_marker, $computer_marker]).size.zero?
+end
 
+def clear_screen
+  system('clear')
+end
+
+def pick_marker!
   loop do
-    display_boards(game_board)
+    print "Which marker would you like to be (X/O)? "
+    marker = gets.chomp.downcase
 
-    player_plays!(game_board)
-    break if someone_won?(game_board) || board_full?(game_board)
+    next unless marker == 'x' || marker == 'o'
 
-    computer_plays!(game_board)
-    break if someone_won?(game_board) || board_full?(game_board)
+    $player_marker = marker.upcase
+    $computer_marker = $player_marker == 'X' ? 'O' : 'X'
+
+    break
+  end
+end
+
+def pick_starter
+  loop do
+    print "Do you want to play first move (Y/n)? "
+    answer = gets.chomp.downcase
+    next unless answer == 'y' || answer == 'n'
+    return answer == 'y' ? :player : :computer
+  end
+end
+
+def play_game(current_player)
+  board = initialize_board
+
+  winner = loop do
+    clear_screen
+    display_boards(board)
+
+    place_piece!(board, current_player)
+    current_player = current_player == :player ? :computer : :player
+
+    winner = find_winner(board)
+    break winner if winner || board_full?(board)
   end
 
-  display_boards(game_board)
-  winner = find_winner(game_board)
-  puts winner ? "Winner is #{winner}" : "It was a tie..."
+  clear_screen
+  display_boards(board)
+  puts '#' * 24
+  puts "*** YAY! YOU WON! :) ***" if winner == $player_marker
+  puts "*** Aww... You Lost! ***" if winner == $computer_marker
+  puts "*** It was a tie.... ***" if winner.nil?
+  puts '#' * 24
 end
 
 def play_again?
@@ -256,7 +232,9 @@ end
 
 def start
   loop do
-    play_game
+    clear_screen
+    pick_marker!
+    play_game(pick_starter)
     next if play_again?
     break
   end
